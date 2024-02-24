f4aa2719bf5c83cae54869e044a03d72230fd910
kokusz19 <kokkusz1996@hotmail.com>
2024-02-24 08:14:39 +0100

    Initialize application

---
+++ src/main/java/com/kokusz19/udinfopark/UdInfoparkApplication.java
@@ -0,0 +1,13 @@
+package com.kokusz19.udinfopark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdInfoparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdInfoparkApplication.class, args);
+	}
+
+}
